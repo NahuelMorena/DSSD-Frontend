0ac4d06983c6ab1fd4b1b0452b99b605123e6ac5
matiasboni <matiasboni01@gmail.com>
2023-11-28 15:04:28 -0300

    Pie chart

---
+++ src/app/modelos/archived-case-dto.spec.ts
@@ -0,0 +1,7 @@
+import { ArchivedCaseDTO } from './archived-case-dto';
+
+describe('ArchivedCaseDTO', () => {
+  it('should create an instance', () => {
+    expect(new ArchivedCaseDTO()).toBeTruthy();
+  });
+});
